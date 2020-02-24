5674265b42cfeb95544c5d546a24c9e4469d6800
Kiril Vatev <vatev.1@gmail.com>
2020-02-24 18:34:16 -0500

    handling shares using a POST form in the service worker

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,50 @@
-/* jshint esversion: 6, browser: true, devel: true */
 /* globals self */
 
-const worker = '👷';
-console.log(worker, 'loaded');
+const VERSION = 'v1.0.0';
+const WORKER = '👷';
+// const KEY = 'reminder-v1';
+
+// eslint-disable-next-line no-console
+const log = (...args) => console.log(WORKER, VERSION, ...args);
+
+const serveShareTarget = event => {
+  // Redirect so the user can refresh the page without resending data.
+  event.respondWith(Response.redirect(event.request.url));
+
+  event.waitUntil(async function () {
+    const data = await event.request.formData();
+    const client = await self.clients.get(event.resultingClientId);
+    const [title, text, url, file] = ['title', 'text', 'url', 'file'].map(n => data.get(n));
+    client.postMessage({ title, text, url, file, action: 'receive-share' });
+  }());
+};
 
 self.addEventListener('install', (event) => {
-  console.log(worker, 'install', event);
+  log('install', event);
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  console.log(worker, 'activate', event);
+  log('activate', event);
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(event) {
-  console.log(worker, 'fetch', event);
-  event.respondWith(fetch(event.request));
+self.addEventListener('sync', function(event) {
+  log('sync', event);
 });
 
-self.addEventListener('sync', function(event) {
-  console.log(worker, 'sync', event);
+self.addEventListener('fetch', (event) => {
+  log('fetch', event.request.method, event.request.url);
+  const url = new URL(event.request.url);
+
+  const isSameOrigin = url.origin === location.origin;
+  const isShareTarget = isSameOrigin && url.searchParams.has('share-target');
+  const isSharePost = isShareTarget && event.request.method === 'POST';
+
+  if (isSharePost) {
+    log('handling share target request');
+    return void serveShareTarget(event);
+  }
+
+  event.respondWith(fetch(event.request));
 });
