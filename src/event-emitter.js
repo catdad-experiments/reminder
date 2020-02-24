465cc4c7f676ee1d3100322c011ad77ff5a3bbfb
Kiril Vatev <vatev.1@gmail.com>
2020-02-24 18:32:23 -0500

    including loader file and helpers from share-edit

---
+++ src/event-emitter.js
@@ -0,0 +1,59 @@
+export default () => {
+  const events = {};
+  const api = {};
+
+  function addEvent(name, func, wrapped) {
+    const evName = name.toLowerCase();
+    events[evName] = events[evName] || [];
+
+    events[evName].push({
+      func: func,
+      wrapped: wrapped
+    });
+
+    return api;
+  }
+
+  function removeEvent(name, func) {
+    events[name] = events[name].filter(function (obj) {
+      return obj.func !== func;
+    });
+
+    return api;
+  }
+
+  api.on = function on(name, func) {
+    return addEvent(name, func, func);
+  };
+
+  api.once = function once(name, func) {
+    return addEvent(name, func, function onceWrapper() {
+      func.apply(null, [].slice.call(arguments));
+      removeEvent(name, func);
+    });
+  };
+
+  api.off = function off(name, func) {
+    const evName = name.toLowerCase();
+
+    if (!events[evName]) {
+      return api;
+    }
+
+    return removeEvent(evName, func);
+  };
+
+  api.emit = function emit(name, ...args) {
+    const evName = name.toLowerCase();
+
+    if (!events[evName]) {
+      return api;
+    }
+
+    events[evName].forEach(function(obj){
+      obj.wrapped(...args);
+    });
+  };
+
+  return api;
+};
---
+++ src/loader.js
@@ -0,0 +1,166 @@
+/* eslint-disable no-console */
+
+//import toast from './toast.js';
+
+let events = (function () {
+  let collection = [];
+
+  return {
+    emit: (...args) => {
+      collection.push(args);
+    },
+    flush: function (emitter) {
+      collection.forEach((args) => {
+        emitter.emit(...args);
+      });
+    }
+  };
+}());
+
+window.addEventListener('beforeinstallprompt', () => {
+  console.log('👀 we can install the app now');
+//  events.emit('can-install', { prompt: ev });
+});
+
+window.addEventListener('appinstalled', () => {
+//  events.emit('info', '🎊 installed 🎊');
+});
+
+if ('serviceWorker' in navigator) {
+  console.log('👍', 'navigator.serviceWorker is supported');
+
+  navigator.serviceWorker.register('./service-worker.js', { scope: './' }).then(() => {
+    console.log('👍', 'worker registered');
+  }).catch(err => {
+    console.warn('👎', 'worker errored', err);
+  });
+
+  navigator.serviceWorker.addEventListener('message', (ev) => {
+    const data = ev.data;
+
+    if (data.action === 'log') {
+      return void console.log('worker:', ...data.args);
+    }
+
+    if (data.action === 'receive-share') {
+      const { text, url, file } = data;
+
+      console.log(text, url, file);
+    }
+
+    console.log('worker message', ev.data);
+  });
+}
+
+export default () => {
+  function onMissingFeatures(missing) {
+    const text = `<p>It seems your browser is not supported. The following features are missing:</p>
+                  <p>${missing.join(', ')}</p>`;
+
+    console.log('TOAST', text);
+    //    toast.error(text, {
+    //      duration: -1 // forever
+    //    });
+  }
+
+  function onError(err, duration = 8 * 1000) {
+    // eslint-disable-next-line no-console
+    console.error(err, duration);
+    //    toast.error(`<p>An error occured:</p><p>${err.toString().split('\n').join('<br/>')}</p>`, {
+    //      duration
+    //    });
+  }
+
+  // detect missing features in the browser
+  const missingFeatures = [
+    'Promise',
+    'Map',
+    'localStorage',
+    ['dynamic import', () => {
+      try {
+        new Function('import("").catch(() => {})')();
+        return true;
+      } catch (err) {
+        return false;
+      }
+    }],
+    ['async/await', () => {
+      try {
+        new Function('async () => {}');
+        return true;
+      } catch (err) {
+        return false;
+      }
+    }]
+  ].filter(function (name) {
+    if (Array.isArray(name)) {
+      const [, test] = name;
+
+      return !test();
+    }
+
+    return !name.split('.').reduce(function (obj, path) {
+      return (obj || {})[path];
+    }, window);
+  }).map(v => Array.isArray(v) ? v[0] : v);
+
+  if (missingFeatures.length) {
+    return onMissingFeatures(missingFeatures);
+  }
+
+  // ------------------------------------------------
+  // we've validated modules... we can use fancy
+  // things now
+  // ------------------------------------------------
+
+  function load(name) {
+    // get around eslint@5 not supporting dynamic import
+    // this is ugly, but I also don't care
+    return (new Function(`return import('${name}')`))().then(m => m.default);
+  }
+
+  async function map(arr, func) {
+    const results = [];
+
+    for (let i = 0; i < arr.length; i++) {
+      results.push(await func(arr[i], i, arr));
+    }
+
+    return results;
+  }
+
+  // load all the modules from the server directly
+  Promise.all([
+    load('./event-emitter.js'),
+  ]).then(async ([
+    eventEmitter,
+    storage,
+    menu,
+    mover,
+    ...modules
+  ]) => {
+    // set up a global event emitter
+    const context = { events: eventEmitter(), menu, mover, storage, load };
+    const destroys = await map(modules, mod => mod(context));
+
+    context.events.on('error', function (err) {
+      onError(err, -1);
+      destroys.forEach(d => d());
+    });
+
+    context.events.on('warn', function (err) {
+      onError(err);
+    });
+
+    context.events.on('info', (msg) => {
+      console.log('TOAST', msg);
+      //      toast.info(msg.toString());
+    });
+
+    events.flush(context.events);
+    events = context.events;
+  }).catch(function catchErr(err) {
+    events.emit('error', err);
+    onError(err);
+  });
+};
