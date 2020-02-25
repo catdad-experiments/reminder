2d17b9586f5285104e5650c83b01f4f07e50fd5f
Kiril Vatev <vatev.1@gmail.com>
2020-02-25 18:43:25 -0500

    handling shared images, starting card style

--- src/receiver.js
+++ src/receiver.js
@@ -12,13 +12,40 @@ const createElem = ({ tag = 'div', children = [], classname }) => {
   return el;
 };
 
+const createImg = ({ file }) => {
+  const url = URL.createObjectURL(file);
+  const img = document.createElement('img');
+
+  img.onload = () => {
+    URL.revokeObjectURL(url);
+  };
+
+  img.src = url;
+
+  return img;
+};
+
 const createText = text => document.createTextNode(text);
 
 export default ({ events }) => {
   const elem = document.querySelector('#main');
 
   const handleFile = file => {
+    const card = createElem({ classname: 'card' });
 
+    card.appendChild(createElem({
+      children: [ createText(`file name: ${file.name}`) ]
+    }));
+    card.appendChild(createElem({
+      children: [ createText(`file size: ${file.size}`) ]
+    }));
+    card.appendChild(createElem({
+      children: [ createText(`file type: ${file.type}`) ]
+    }));
+
+    card.appendChild(createImg({ file }));
+
+    elem.appendChild(card);
   };
 
   const onShare = ({ title, text, url, file }) => {
@@ -26,18 +53,19 @@ export default ({ events }) => {
       return handleFile(file);
     }
 
-    elem.appendChild(createElem({
-      classname: 'card',
+    const card = createElem({ classname: 'card' });
+
+    card.appendChild(createElem({
       children: [ createText(`title: ${title}`) ]
     }));
-    elem.appendChild(createElem({
-      classname: 'card',
+    card.appendChild(createElem({
       children: [ createText(`text: ${text}`) ]
     }));
-    elem.appendChild(createElem({
-      classname: 'card',
+    card.appendChild(createElem({
       children: [ createText(`url: ${url}`) ]
     }));
+
+    elem.appendChild(card);
   };
 
   events.on('receive-share', onShare);
