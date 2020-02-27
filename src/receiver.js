fc17fb9c3e6ebd3c96b79a40e8b7e65d64134489
Kiril Vatev <vatev.1@gmail.com>
2020-02-27 18:23:42 -0500

    new shared are shown on an editable overlay first, allowing the user to change them or cancel

--- src/receiver.js
+++ src/receiver.js
@@ -1,19 +1,51 @@
-export default ({ events, db }) => {
-  const saveFile = async file => {
+/* eslint-disable no-console */
+
+export default ({ events, db, dom }) => {
+  const editable = (elem) => dom.props(elem, { contenteditable: '' });
+
+  const savePlainShare = async ({ title, text, url }) => {
+    const result = await db.save({ title, text, url });
+    console.log(result);
+  };
+
+  const saveFileShare = async ({ file }) => {
 
   };
 
+  const splash = ({ title, text, url, file }) => {
+    const TITLE = editable(dom.children(dom.div('title'), dom.text(title)));
+    const TEXT = editable(dom.children(dom.div('text'), dom.text(text)));
+    const URL = editable(dom.children(dom.div('url'), dom.text(url)));
+
+    const elem = dom.children(
+      dom.div('splash'),
+      TITLE, TEXT, URL,
+      dom.children(
+        dom.div(),
+        dom.button('Cancel', () => {
+          elem.remove();
+        }),
+        dom.button('Save', () => {
+          const title = TITLE.innerText;
+          const text = TEXT.innerText;
+          const url = URL.innerText;
+
+          savePlainShare({ title, text, url }).then(() => {
+            events.emit('render');
+          }).catch(e => {
+            console.error(e);
+          }).then(() => {
+            elem.remove();
+          });
+        })
+      )
+    );
+
+    document.body.appendChild(elem);
+  };
+
   const onShare = async ({ title, text, url, file }) => {
-    if (file) {
-      await saveFile(file);
-      return;
-    }
-
-    const result = await db.save({ title, text, url });
-
-    console.log(result);
-
-    events.emit('render');
+    splash({ title, text, url, file });
   };
 
   events.on('receive-share', onShare);
