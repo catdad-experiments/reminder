557996817f2f2040a32d0f9c5ad0897ff332a3d1
Kiril Vatev <vatev.1@gmail.com>
2020-02-27 19:05:23 -0500

    allowing creating a sample share with a file in it

--- src/sample.js
+++ src/sample.js
@@ -6,7 +6,7 @@ const samples = {
   }
 };
 
-export default ({ events }) => {
+export default ({ events, dom }) => {
   const query = window.location.search.substring(1).split('&').reduce((memo, item) => {
     const [key, ...value] = item.split('=');
     memo[key] = value.join('=');
@@ -14,7 +14,32 @@ export default ({ events }) => {
     return memo;
   }, {});
 
+  const sampleFileShare = () => {
+    const input = dom.props(dom.elem('input'), { type: 'file' });
+    const splash = dom.children(
+      dom.classname(dom.div(), 'splash', 'limit'),
+      dom.children(dom.div(), dom.text('Select a file to show sample share')),
+      input
+    );
+
+    document.body.appendChild(splash);
+
+    input.onchange = (ev) => {
+      const file = ev.target.files[0];
+
+      splash.remove();
+
+      if (file) {
+        events.emit('receive-share', { file });
+      }
+    };
+  };
+
   const timer = setTimeout(() => {
+    if (query.sample === 'file') {
+      return sampleFileShare();
+    }
+
     if (query.sample && samples[query.sample]) {
       events.emit('receive-share', samples[query.sample]);
     }
