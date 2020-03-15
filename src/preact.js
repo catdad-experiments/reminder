bd394b7d24e0eb2f9a966afc233758be1e4bf50d
Kiril Vatev <vatev.1@gmail.com>
2020-03-15 17:51:23 -0400

    using real preact instead of the version packaged into htm

--- src/preact.js
+++ src/preact.js
@@ -1,9 +1,8 @@
-import { html, render } from 'https://cdn.jsdelivr.net/npm/htm@3.0.3/preact/standalone.module.js';
-import { Fragment } from 'https://cdn.jsdelivr.net/npm/preact@10.3.4/dist/preact.module.js';
+//import { html, render } from 'https://cdn.jsdelivr.net/npm/htm@3.0.3/preact/standalone.module.js';
+//import { Fragment } from 'https://cdn.jsdelivr.net/npm/preact@10.3.4/dist/preact.module.js';
 
-//import { h, render, Fragment } from 'https://cdn.jsdelivr.net/npm/preact@10.3.4/dist/preact.module.js';
-//import htm from 'https://cdn.jsdelivr.net/npm/htm@3.0.3/dist/htm.module.js';
-//// Initialize htm with Preact
-//const html = htm.bind(h);
+import { h, render, Fragment } from 'https://cdn.jsdelivr.net/npm/preact@10.3.4/dist/preact.module.js';
+import htm from 'https://cdn.jsdelivr.net/npm/htm@3.0.3/dist/htm.module.js';
+const html = htm.bind(h);
 
 export { html, render, Fragment };
