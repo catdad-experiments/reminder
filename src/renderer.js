7f31461bd8ef08e91974757c992199c673694740
Kiril Vatev <vatev.1@gmail.com>
2020-03-16 17:22:03 -0400

    replacing old renderer with preact-based renderer

--- src/renderer.js
+++ src/renderer.js
@@ -1,3 +1,7 @@
+/* eslint-disable indent, no-console */
+
+import { html, render, Fragment } from './preact.js';
+
 const noErr = prom => prom.catch(e => {
   // eslint-disable-next-line no-console
   console.error('Hanlded Error:', e);
@@ -9,155 +13,144 @@ const dateString = date => {
   return `${day}, ${date.toLocaleDateString()}, ${hour}`;
 };
 
-export default ({ events, db, dom, notification }) => {
+const ImageCard = record => {
+  const url = URL.createObjectURL(new Blob([record.filebuffer]));
+
+  return html`
+    <${Fragment} key=fragment${record.id}>
+      <img key=img${record.id} src=${url} onload=${() => {
+        console.log('revoke url', record.id);
+        URL.revokeObjectURL(url);
+      }} />
+      <div class=text>
+        ${record.filename} (${record.filetype})
+      </div>
+    <//>
+  `;
+};
+
+const Field = ({ value, ...rest }) => {
+  if (!value) {
+    return html``;
+  }
+
+  return html`<div ...${rest}>
+    ${value.split('\n').map(i => i.trim()).map(line => {
+      const urls = line.match(/[a-z]+:\/\/[^ ]+/ig);
+
+      if (!urls) {
+        return html`<p>${line}</p>`;
+      }
+
+      const lineElements = [];
+      let tempLine = line;
+
+      for (let url of urls) {
+        const [before, ...rest] = tempLine.split(url);
+
+        lineElements.push(html`<span>${before}</span>`);
+        lineElements.push(html`<a href=${url}>${url}</a>`);
+        tempLine = rest.join(url);
+      }
+
+      if (tempLine) {
+        lineElements.push(html`<span>${tempLine}</span>`);
+      }
+
+      return html`<p>${lineElements}</p>`;
+    })}
+  </div>`;
+};
+
+const NoteCard = record => {
+  const { title, text, url } = record;
+
+  return html`
+    <${Fragment}>
+      <${Field} class=title value=${title} />
+      <${Field} class=text value=${text} />
+      <${Field} class=text value=${url} />
+    <//>
+  `;
+};
+
+const Card = record => {
+  return html`
+    <${Fragment}>
+      <${record.filebuffer ? ImageCard : NoteCard} ...${record} />
+    <//>
+  `;
+};
+
+const ReminderDate = ({ remindAt }) => {
+  return html`<span class=date>${remindAt ? dateString(new Date(remindAt)) : 'Never'}</span>`;
+};
+
+const Icon = ({ name, ...rest }) => {
+  return html`<i class=material-icons ...${rest}>${name}</i>`;
+};
+
+export default ({ events, db, notification }) => {
   const elem = document.querySelector('#main');
   let FOCUS_ID;
 
-  const deleteCard = async (card, id) => {
-    card.remove();
-    noErr(db.remove({ id }));
-    events.emit('render-complete');
-  };
-
-  const renderDate = remindAt => dom.classname(
-    dom.span(remindAt ? dateString(new Date(remindAt)) : 'Never'),
-    'date'
-  );
-
-  const renderField = field => {
-    if (!field) {
-      return dom.nill();
-    }
-
-    return dom.fragment(
-      ...field.split('\n').map(i => i.trim()).map(line => {
-        const urls = line.match(/[a-z]+:\/\/[^ ]+/ig);
-
-        if (!urls) {
-          return dom.p(line);
-        }
-
-        const lineElements = [];
-        let tempLine = line;
-
-        for (let url of urls) {
-          const [before, ...rest] = tempLine.split(url);
-
-          lineElements.push(dom.span(before));
-          lineElements.push(dom.link(url, url));
-          tempLine = rest.join(url);
-        }
-
-        if (tempLine) {
-          lineElements.push(dom.span(tempLine));
-        }
-
-        return dom.children(
-          dom.p(),
-          dom.fragment(...lineElements)
-        );
-      })
-    );
-  };
-
-  const renderNote = ({ title, text, url }) => {
-    return dom.fragment(
-      title ? dom.children(dom.div('title'), renderField(title)) : dom.nill(),
-      text ? dom.children(dom.div('text'), renderField(text)) : dom.nill(),
-      url ? dom.children(dom.div('text'), renderField(url)) : dom.nill(),
-    );
-  };
-
-  const renderFile = ({ filebuffer, filename, filetype }) => {
-    return dom.fragment(
-      dom.img(new Blob([filebuffer])),
-      dom.children(
-        dom.div('text'),
-        dom.text(`${filename} (${filetype})`)
-      )
-    );
-  };
-
-  const getCards = () => [].slice.call(document.querySelectorAll('.card'))
-    .map(card => {
-      return {
-        card, id: Number(card.getAttribute('data-id'))
-      };
-    });
-
-  const applyCardFocus = () => {
-    getCards().forEach(({ card, id }) => {
-      if (id !== FOCUS_ID) {
-        return void card.classList.remove('focused');
-      }
-
-      card.classList.add('focused');
-      card.scrollIntoView({ block: 'center', behavior: 'smooth' });
-    });
-  };
-
   const onRender = async () => {
-    const cards = getCards();
+    console.time('preact render');
 
-    let currentCard;
+    const notifications = (await notification.get()).reduce((m, n) => {
+      m[n.id] = n;
+      return m;
+    }, {});
+
+    console.log(notifications);
+
+    const children = [];
 
     await db.each(null, record => {
-      const idx = cards.findIndex(c => c.id === record.id);
       const isFile = 'filebuffer' in record;
 
-      if (idx !== -1) {
-        currentCard = cards[idx];
-        return;
-      }
+      children.push(html`
+        <div key=card${record.id} class=${['card'].concat(record.id === FOCUS_ID ? ['focused'] : []).join(' ')} data-id=${record.id}>
+          <${Card} ...${record} />
+          <div class=buttons>
+            <${ReminderDate} remindAt=${record.remindAt} />
+            <${Icon} name=notifications_active onClick=${async (e) => {
+              e.stopPropagation();
+              await noErr(notification.show(record));
+            }}/>
+            ${isFile ? null : html`<${Icon} name=share onClick=${async (e) => {
+              e.stopPropagation();
 
-      const card = dom.props(dom.div('card'), {
-        'data-id': record.id
-      });
+              const data = {};
+              record.title && (data.title = record.title);
+              record.text && (data.text = record.text);
+              record.url && (data.url = record.url);
 
-      dom.children(
-        card,
-        isFile ? renderFile(record) : renderNote(record),
-        dom.children(
-          dom.div('buttons'),
-          renderDate(record.remindAt),
-          dom.click(dom.icon('notifications_active'), async (e) => {
-            e.stopPropagation();
-            await notification.show(record);
-          }),
-          navigator.share && !isFile ? dom.click(dom.icon('share'), async (e) => {
-            e.stopPropagation();
-
-            const data = {};
-            record.title && (data.title = record.title);
-            record.text && (data.text = record.text);
-            record.url && (data.url = record.url);
-
-            await noErr(navigator.share(data));
-          }) : dom.nill(),
-          dom.click(dom.icon('delete'), async () => {
-            deleteCard(card, record.id);
-            await noErr(notification.close(record.id));
-          })
-        )
-      );
-
-      dom.children(card, dom.div('focus-inset'));
-
-      if (currentCard) {
-        elem.insertBefore(card, currentCard.card);
-      } else {
-        elem.insertBefore(card, elem.firstChild);
-      }
+              await noErr(navigator.share(data));
+            }}/>`}
+            <${Icon} name=delete onClick=${async (e) => {
+              e.stopPropagation();
+              await noErr(db.remove({ id: record.id }));
+              await noErr(onRender());
+            }}/>
+          </div>
+          <div class=focus-inset></div>
+        </div>
+      `);
     });
 
-    applyCardFocus();
+    console.time('preact');
+    render(html`<${Fragment}>${children.reverse()}<//>`, elem);
+    console.timeEnd('preact');
+
+    console.timeEnd('preact render');
 
     events.emit('render-complete');
   };
 
   const onRenderFocus = ({ id }) => {
     FOCUS_ID = id;
-    applyCardFocus();
+    onRender();
   };
 
   events.on('render', onRender);
